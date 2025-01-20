b8d5624cc540d4c2e6a0f2dc407afa6013aec5bb
focandlol <focandlol@naver.com>
2025-01-20 18:09:12 +0900

    merge test

--- src/main/java/focandlol/test/TestController.java
+++ src/main/java/focandlol/test/TestController.java
@@ -5,5 +5,7 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
+
+        System.out.println("merge test");
     }
 }
